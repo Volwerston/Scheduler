ea96b636e2fa7b8c080fae1cd98fed32d4b4351a
User <User@WIN-BTDCN99RQAA>
2017-05-22 02:47:59 +0300

    Start page ui layout

--- Scheduler/Scheduler/App_Start/RouteConfig.cs
+++ Scheduler/Scheduler/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Scheduler
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Main", action = "StartPage", id = UrlParameter.Optional }
             );
         }
     }
---
+++ Scheduler/Scheduler/Controllers/MainController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Scheduler.Controllers
+{
+    public class MainController : Controller
+    {
+        // GET: Main
+        public ActionResult StartPage()
+        {
+            return View();
+        }
+    }
+}
