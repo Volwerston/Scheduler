99e5116ddc39c49fb975e90038e17aedcea094ab
User <User@WIN-BTDCN99RQAA>
2017-05-22 09:25:51 +0300

    Ready start page ui

---
+++ Scheduler/Scheduler/Models/Auxiliary/StartPageBlockData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Scheduler.Models.Auxiliary
+{
+    public class StartPageBlockData
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ColorName { get; set; }
+        public string NextBlock { get; set; }
+        public string ArrowImageSrc { get; set; }
+    }
+}
